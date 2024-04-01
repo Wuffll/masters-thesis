20ca3c913a09063e2b6d963db5bf86fb9baff0ed
Adam Ergotić <adam.ergotic@outlook.com>
2024-04-01 13:41:30 +0200

    Stopwatch class
    Added a Stopwatch class to time events in the application

---
+++ Project/headers/Stopwatch.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <chrono>
+
+#define CLOCK std::chrono::high_resolution_clock
+
+class Stopwatch
+{
+public:
+
+	Stopwatch() = default;
+
+	void start();
+	const double& stop();
+	const double& lap();
+
+	const double& getTimeElapsed() const;
+
+private:
+
+	bool m_HasStarted = false;
+
+	std::chrono::time_point<CLOCK> m_Start;
+	std::chrono::time_point<CLOCK> m_End;
+
+	double m_TimeElapsed = 0.0; // in seconds
+
+};
+
---
+++ Project/src/Stopwatch.cpp
@@ -0,0 +1,51 @@
+#include "Stopwatch.h"
+
+#include "Debug.h"
+
+using ms = std::chrono::milliseconds;
+using dsec = std::chrono::duration<double>;
+
+void Stopwatch::start()
+{
+	if (m_HasStarted)
+		Debug::printMessage(*this, "Restarting stopwatch!", DebugSeverityLevel::LOW);
+
+	m_Start = CLOCK::now();
+
+	m_HasStarted = true;
+}
+
+const double& Stopwatch::stop()
+{
+	if (!m_HasStarted)
+	{
+		Debug::printMessage(*this, "The stopwatch wasn't running!", DebugSeverityLevel::MEDIUM);
+		return 0.0;
+	}
+
+	m_End = CLOCK::now();
+
+	dsec durationInSeconds = m_End - m_Start;
+
+	m_TimeElapsed = durationInSeconds.count();
+
+	return m_TimeElapsed;
+}
+
+const double& Stopwatch::lap()
+{
+	m_End = CLOCK::now();
+
+	dsec durationInSeconds = m_End - m_Start;
+
+	m_TimeElapsed = durationInSeconds.count();
+
+	m_Start = m_End;
+
+	return m_TimeElapsed;
+}
+
+const double& Stopwatch::getTimeElapsed() const
+{
+	return m_TimeElapsed;
+}
